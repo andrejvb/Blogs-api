85007d8780ed259c20f5419458c297f779624341
andrejvb <andre.jvb@gmail.com>
2023-05-23 12:18:32 -0300

    finally twelfth fix

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,12 +1,11 @@
-const services = require('../services');
+const service = require('../services');
 const { generateToken } = require('../auth/JWT');
 
 const findEmailAndPassword = async (req, res) => {
     const { email, password } = req.body;
-    const user = await services.getByEmailAndPassword(email, password);
-    const { password: _password, ...userWithouthPassword } = user.dataValues;
-    if (user.message) return res.status(400).json({ message: user.message });
-    const token = generateToken({ data: userWithouthPassword });
+    const result = await service.emailAndPassword(email, password);
+    if (result.message) return res.status(400).json({ message: result.message });
+    const token = generateToken(email);
 
     return res.status(200).json({ token });
 };
--- src/middlewares/tokenvalidation.js
+++ src/middlewares/tokenvalidation.js
@@ -8,12 +8,13 @@ const tokenValidation = async (req, res, next) => {
     try {
         const { authorization: token } = req.headers;
         if (!token) return res.status(badReqCode).json({ message: 'Token not found' });
-        jwt.verify(token, secret);
-        next();
+        const data = jwt.verify(token, secret);  
+        req.payload = data;      
     } catch (error) {
         console.error(error);
        return res.status(badReqCode).json({ message: 'Expired or invalid token' });
-    }    
+    } 
+    next();   
 };
 
 module.exports = { tokenValidation };
